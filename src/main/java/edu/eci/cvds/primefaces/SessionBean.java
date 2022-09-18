6d7cd6376c90d1219474639b88571c5ebcbb7957
Andres Ariza <andres-ariza70@hotmail.com>
2022-09-18 15:07:21 -0500

    Se actualizó la lógica del juego

--- src/main/java/edu/eci/cvds/primefaces/SessionBean.java
+++ src/main/java/edu/eci/cvds/primefaces/SessionBean.java
@@ -2,39 +2,56 @@ package edu.eci.cvds.primefaces;
 
 import javax.faces.bean.ManagedBean;
 import javax.enterprise.context.ApplicationScoped;
+import javax.faces.bean.SessionScoped;
+import java.util.ArrayList;
+import java.util.List;
 
-@ManagedBean(name = "guessName")
-@ApplicationScoped
+@ManagedBean(name = "guessBean")
+@SessionScoped
 public class SessionBean {
 
     int guessNumber;
     int attempts;
     int rewardPrice;
     String status;
+    List<Integer> lastAttempts;
 
     public SessionBean() {
-        this.guessNumber = 7;
-        this.attempts = 0;
-        this.rewardPrice = 100000;
-        this.status = "no";
+        restart();
     }
 
     public void restart() {
-        this.guessNumber = 7;
+        this.guessNumber = (int)Math.floor(Math.random()*(10)+0);
         this.attempts = 0;
         this.rewardPrice = 100000;
         this.status = "no";
+        this.lastAttempts = new ArrayList<Integer>();
     }
 
     public void guess(int number) {
         if (number == this.guessNumber) {
-            this.status = "yes"+rewardPrice;
+            this.status = "yes "+rewardPrice;
         } else {
             this.attempts++;
             this.rewardPrice -= 10000;
+            lastAttempts.add(number);
         }
     }
 
+    public String getLastAttempts() {
+        String log = "";
+        for (int i : lastAttempts) {
+            log += i+", ";
+        }
+        return log;
+    }
+
+    public void makeGuess(String a) {
+        //System.out.println(toString());
+        guess(Integer.parseInt(a));
+        //System.out.println(toString()+"\n");
+
+    }
     public int getGuessNumber() {
         return guessNumber;
     }
@@ -65,5 +82,15 @@ public class SessionBean {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "SessionBean{" +
+                "guessNumber=" + guessNumber +
+                ", attempts=" + attempts +
+                ", rewardPrice=" + rewardPrice +
+                ", status='" + status + '\'' +
+                '}';
     }
 }
